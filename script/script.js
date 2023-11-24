b9a508739a6a0b771febaa22406dd2564b4684ad
UmbertoSerino <serumberto@gmail.com>
2023-11-24 16:43:25 +0100

    created folder and added array with objects

---
+++ script/script.js
@@ -0,0 +1,30 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede, potete usare come esempio lo screenshot,  ma anche realizzare qualcosa di creativo!!!
+// Consigli del giorno:
+// Ragioniamo come sempre a step. Prima la logica in italiano e poi traduciamo in codice.
+// E ricordiamoci che console.log() è nostro amico!
+
+myTeam [
+    {name : "Gianluigi-Buffon", position : "Goalkeeper", picture : ""},
+    {name : "Roberto-Carlos", position : "Defender", picture : ""},
+    {name : "Fabio-Cannavaro", position : "Defender", picture : ""},
+    {name : "Paolo-Maldini", position : "Defender", picture : ""},
+    {name : "Gianluca-Zambrotta", position : "Defender", picture : ""},
+    {name : "Patrick-Vieira", position : "Midfielder", picture : ""},
+    {name : "JaunSebastian-Veron", position : "Midfielder", picture : ""},
+    {name : "Zinedine-Zidane", position : "Midfielder", picture : ""},
+    {name : "David-Beckham", position : "Midfielder", picture : ""},
+    {name : "LuisNazarioDeLima-Ronaldo", position : "Forward", picture : ""},
+    {name : "Andrij-Shevchenko", position : "Forward", picture : ""},
+]
+
